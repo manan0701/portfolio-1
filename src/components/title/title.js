9340324db049393b82fa668727df391272db7125
filahf <filahf94@gmail.com>
2020-01-08 16:24:35 +0100

    cname

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -13,7 +13,9 @@ function Title() {
             <p style={{ color: '#434c5e' }}>
                 Hello, I'm Filip.
                 <br />
-                I'm a M.Sc. Student at Faculty of Engineering LTH. Currently, IT Consult at <a className="link" href="http://www.lunicore.se/">Lunicore.</a> To keep in touch, simply <a className="link" href="mailto:filahf94@gmail.com">email</a> or add me on <a className="link" href="https://www.linkedin.com/in/filip-%C3%A5hfelt-22977119a/">LinkedIn</a>.
+                I'm a M.Sc. Student at Faculty of Engineering LTH. Currently, IT Consult at <a className="link" href="http://www.lunicore.se/">Lunicore.</a>
+                <br />
+                To keep in touch, simply <a className="link" href="mailto:filahf94@gmail.com">email</a> or add me on <a className="link" href="https://www.linkedin.com/in/filip-%C3%A5hfelt-22977119a/">LinkedIn</a>.
             </p>
         </div>
     </div>
