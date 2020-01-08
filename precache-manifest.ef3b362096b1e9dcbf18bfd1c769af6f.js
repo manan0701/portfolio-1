5984bfbd7c019ad96b4435b18fa066b84cd02cba
filahf <filahf94@gmail.com>
2020-01-08 16:07:52 +0100

    Updates

---
+++ precache-manifest.ef3b362096b1e9dcbf18bfd1c769af6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebc08bac3bbecf3e892cc04c949a41e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ea36061a550c186a65d0",
+    "url": "/portfolio/static/css/main.48052cb9.chunk.css"
+  },
+  {
+    "revision": "fdf0156401f6eb52fba0",
+    "url": "/portfolio/static/js/2.70866c63.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.70866c63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea36061a550c186a65d0",
+    "url": "/portfolio/static/js/main.bef2d8f8.chunk.js"
+  },
+  {
+    "revision": "e7e9630300561249d4d9",
+    "url": "/portfolio/static/js/runtime-main.680e3ffb.js"
+  }
+]);
