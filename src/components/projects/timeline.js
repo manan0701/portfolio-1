7e91c5af410ecc746aae3d597f1044eafafbf219
filahf <filahf94@gmail.com>
2020-02-01 11:35:04 +0100

    abstraction of data

--- src/components/projects/timeline.js
+++ src/components/projects/timeline.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import './projects.scss'
+import './projects.scss';
+
 
 function Timeline() {
     return (
+        
         <VerticalTimeline>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export default {
+    projects: [
+        {            
+            title: "Personal Website",
+            tech: ["React", "Sass"],
+            desc: "The website you're currently browsing!"
+        }
+    ]
+}
